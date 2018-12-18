78203d323495fef1d05f460316d6741f8ee2aacf
Urbenson Laurent <ulaurent12@gmail.com>
2018-12-17 20:22:40 -0500

    new Net Force Exerted on planet method in X and Y dir

--- Planet.java
+++ Planet.java
@@ -18,6 +18,8 @@ public class Planet{
 
     public String imgFileName;
 
+    static final double G = 6.67e-11;
+
     public Planet (double xP, double yP, double xV, double yV, double m, String img ){
         xxPos = xP;
         yyPos = yP;
@@ -49,13 +51,72 @@ public class Planet{
 
     public double calcForceExertedBy (Planet planetIn){
 
-        double g = 6.67e-11;
         double r = calcDistance(planetIn);
-        double f = ((g)*(this.mass)*(planetIn.mass))/ (r*r);
+        double f = ((G)*(this.mass)*(planetIn.mass)) / (r*r);
 
         return f;
     };
-    
+
+    public double calcForceExertedByX (Planet planetIn){
+
+        double f = calcForceExertedBy(planetIn);
+
+        double dx = ((planetIn.xxPos) - (this.xxPos));
+
+        double r = calcDistance(planetIn);
+
+        double fX = ((f)*(dx)) / (r);
+
+        return fX;
+    };
+
+    public double calcForceExertedByY (Planet planetIn){
+
+        double f = calcForceExertedBy(planetIn);
+
+        double dy = ((planetIn.yyPos) - (this.yyPos));
+
+        double r = calcDistance(planetIn);
+
+        double fY = ((f)*(dy)) / (r);
+
+        return fY;
+    };
+
+    public double calcNetForceExertedByX(Planet [] planetIn){
+
+        double fNetX = 0;
+
+        for (Planet each : planetIn){
+            if (this.xxPos == each.xxPos && this.yyPos == each.yyPos){
+              continue;
+            }
+            else{
+                fNetX += calcForceExertedByX(each);
+            }
+        }
+
+        return fNetX;
+
+    };
+
+    public double calcNetForceExertedByY(Planet [] planetIn){
+
+        double fNetY = 0;
+
+        for (Planet each : planetIn){
+            if (this.xxPos == each.xxPos && this.yyPos == each.yyPos){
+                continue;
+            }
+            else{
+                fNetY += calcForceExertedByY(each);
+            }
+        }
+
+        return fNetY;
+
+    };
+
 
 
 }
